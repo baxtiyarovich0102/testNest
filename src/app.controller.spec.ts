c637aa6cf60bf8036f40a45ccb85edaa4b433954
Shahriyor <baxtiyarovich0102@gmail.com>
2025-06-14 18:46:33 +0500

    wty

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello CI!');
+      expect(appController.getHello()).toBe('Hello CI and CD!');
     });
   });
 });
